3a85c71fb059d14eba60d4860fa68199f04f621b
MattLane6962 <umbranival@gmail.com>
2022-10-26 03:37:08 -0500

    Add project files.

---
+++ Modify Employees On MongoDB/Employee.cs
@@ -0,0 +1,37 @@
+﻿namespace Modify_Employees_On_MongoDB
+using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace com.hr
+{
+    class Employee
+    {
+        public string First
+        { get; set; }
+
+        public string Last
+        { get; set; }
+
+        public decimal Salary
+        { get; set; }
+
+        public ObjectId Id
+        { get; set; }
+
+        public override string ToString() =>
+        "Employee [id =" + Id + ", Firstname=" + First + ", lastname=" +
+        Last + ", salary= " + Salary + "]";
+
+        public static void printCollection(IMongoCollection<Employee> collection)
+        {
+            var emps = collection.Find(_ => true).ToList();
+            foreach (Employee emp in emps)
+            {
+                Console.WriteLine(emp);
+            }
+        }
+
+    }
+}
---
+++ Modify Employees On MongoDB/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MongoDB.Driver;
+
+using com.hr; // Needed for Employee
+
+namespace listlinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase("hr");
+            var collection = db.GetCollection<Employee>("emp");
+            //Employee.printCollection(collection);
+
+            //var employee = new Employee{First="Samuel", Last= "Clayton", Salary= 1000000};
+            //collection.InsertOne(employee);
+            var filter = Builders<Employee>.Filter.Eq("Last", "Clayton") &
+                        Builders<Employee>.Filter.Eq("First", "Samuel");
+
+            var update = Builders<Employee>.Update.Set("Salary", 1500000);
+            var opts = new UpdateOptions() { IsUpsert = true };
+            //collection.DeleteMany(filter);
+            collection.UpdateOne(filter, update, opts);
+
+            filter = Builders<Employee>.Filter.Eq("Last", "Miller") &
+                       Builders<Employee>.Filter.Eq("First", "Heather");
+
+            update = Builders<Employee>.Update.Set("Salary", 1000000);
+            collection.UpdateOne(filter, update);
+            Employee.printCollection(collection);
+
+            //db.DropCollection("emp");
+        }
+    }
+}
